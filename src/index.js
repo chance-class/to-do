de7af4ce63abdc1c57ff8e3bcee731d46c4b8d89
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-26 17:00:58 -0500

    Add ELs for pop-up btns

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import './style.css';
 
 const add = document.querySelector(".add-btn");
-const popupToDo = document.querySelector(".pop-up-td");
+const popupTD = document.querySelector(".pop-up-td");
+const popupProj = document.querySelector(".pop-up-proj");
+const popupNote = document.querySelector(".pop-up-note");
 add.addEventListener("click", () => {
-  popupToDo.classList.add("display");
+  popupTD.style.display = "block";
+  popupProj.style.display = "none";
+  popupNote.style.display = "none";
+})
+const addTD = document.querySelector(".add-td");
+addTD.addEventListener("click", () => {
+    popupTD.style.display = "block";
+  popupProj.style.display = "none";
+  popupNote.style.display = "none";
+})
+const addProj = document.querySelector(".add-proj");
+addProj.addEventListener("click", () => {
+    popupProj.style.display = "block";
+  popupNote.style.display = "none";
+  popupTD.style.display = "none";
+})
+const addNote = document.querySelector(".add-note");
+addNote.addEventListener("click", () => {
+    popupNote.style.display = "block";
+  popupTD.style.display = "none";
+  popupProj.style.display = "none";
 })
