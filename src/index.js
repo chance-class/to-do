8884db9eb09d4d4b7864fc393e59f6fc311db09f
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-26 17:19:59 -0500

    Format pop-up forms

--- src/index.js
+++ src/index.js
@@ -5,25 +5,25 @@ const popupTD = document.querySelector(".pop-up-td");
 const popupProj = document.querySelector(".pop-up-proj");
 const popupNote = document.querySelector(".pop-up-note");
 add.addEventListener("click", () => {
-  popupTD.style.display = "block";
+  popupTD.style.display = "grid";
   popupProj.style.display = "none";
   popupNote.style.display = "none";
 })
 const addTD = document.querySelector(".add-td");
 addTD.addEventListener("click", () => {
-    popupTD.style.display = "block";
+    popupTD.style.display = "grid";
   popupProj.style.display = "none";
   popupNote.style.display = "none";
 })
 const addProj = document.querySelector(".add-proj");
 addProj.addEventListener("click", () => {
-    popupProj.style.display = "block";
+    popupProj.style.display = "grid";
   popupNote.style.display = "none";
   popupTD.style.display = "none";
 })
 const addNote = document.querySelector(".add-note");
 addNote.addEventListener("click", () => {
-    popupNote.style.display = "block";
+    popupNote.style.display = "grid";
   popupTD.style.display = "none";
   popupProj.style.display = "none";
 })
