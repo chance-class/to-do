9edf2aa77c1158c98ff88e602bc64dd0efe88409
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-26 19:24:07 -0500

    Add select pop-up

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 import './style.css';
 
 const add = document.querySelector(".add-btn");
+const popupSelect = document.querySelector(".pop-up-select");
 const popupTD = document.querySelector(".pop-up-td");
 const popupProj = document.querySelector(".pop-up-proj");
 const popupNote = document.querySelector(".pop-up-note");
 add.addEventListener("click", () => {
-  popupTD.style.display = "grid";
+  popupSelect.style.display = "grid";
   popupProj.style.display = "none";
   popupNote.style.display = "none";
+  popupTD.style.display = "none";
 })
 const addTD = document.querySelector(".add-td");
 addTD.addEventListener("click", () => {
-    popupTD.style.display = "grid";
+  popupTD.style.display = "grid";
+  popupSelect.style.display = "none";
   popupProj.style.display = "none";
   popupNote.style.display = "none";
 })
 const addProj = document.querySelector(".add-proj");
 addProj.addEventListener("click", () => {
-    popupProj.style.display = "grid";
+  popupProj.style.display = "grid";
+  popupSelect.style.display = "none";
   popupNote.style.display = "none";
   popupTD.style.display = "none";
 })
 const addNote = document.querySelector(".add-note");
 addNote.addEventListener("click", () => {
-    popupNote.style.display = "grid";
+  popupNote.style.display = "grid";
+  popupSelect.style.display = "none";
   popupTD.style.display = "none";
   popupProj.style.display = "none";
 })
